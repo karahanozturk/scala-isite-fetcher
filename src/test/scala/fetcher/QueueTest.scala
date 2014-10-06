ed50416cf1b9e9f65c479533fb8cb4fad9007ead
karahanozturk <karahanozturk@gmail.com>
2014-10-06 14:32:06 +0100

    refactor tests

--- src/test/scala/fetcher/QueueTest.scala
+++ src/test/scala/fetcher/QueueTest.scala
@@ -11,25 +11,26 @@ import scala.concurrent.Await
 import scala.io.Source
 
 class QueueTest extends Specification with Mockito {
+  isolated
 
-  val sqs = mock[AmazonSQSClient]
-  val qConf = QueueConfig("url", 1, 1, 1)
-  val queue = new Queue(sqs, qConf)
+  "Queue" >> {
+    val sqs = mock[AmazonSQSClient]
+    val qConf = QueueConfig("url", 1, 1, 1)
+    val queue = new Queue(sqs, qConf)
 
-  val sqsMsg = Source.fromURL(getClass.getResource("/snsNotification")).mkString
-  val request = new ReceiveMessageRequest(qConf.url).withWaitTimeSeconds(qConf.waitTimeSeconds).withMaxNumberOfMessages(qConf.maxNumberOfMessages)
-  val sqsMessages = new util.ArrayList[Message]
-  val message = new model.Message().withReceiptHandle("receiptHandle").withBody(sqsMsg)
-  val result = new ReceiveMessageResult().withMessages(message)
-  sqs.receiveMessage(any[ReceiveMessageRequest]) returns result
+    val sqsMsg = Source.fromURL(getClass.getResource("/snsNotification")).mkString
+    val request = new ReceiveMessageRequest(qConf.url).withWaitTimeSeconds(qConf.waitTimeSeconds).withMaxNumberOfMessages(qConf.maxNumberOfMessages)
+    val sqsMessages = new util.ArrayList[Message]
+    val message = new model.Message().withReceiptHandle("receiptHandle").withBody(sqsMsg)
+    val result = new ReceiveMessageResult().withMessages(message)
+    sqs.receiveMessage(any[ReceiveMessageRequest]) returns result
 
-  "Queue" should {
-    "extract notification from SQS message" in {
+    "extract notification from SQS message" >> {
       val msg = Await.result(queue.pollMessage(), scala.concurrent.duration.DurationInt(1000).millis)
       msg must equalTo(Message("iplayer", "publish", "e56fce55-86cc-4f71-aa67-3157ddb70286", "receiptHandle"))
     }
 
-    "delete message from the SQS" in{
+    "delete message from the SQS" >> {
       val msg = Message("iplayer", "publish", "e56fce55-86cc-4f71-aa67-3157ddb70286", "receiptHandle")
       Await.result(queue.deleteMessage(msg), scala.concurrent.duration.DurationInt(1000).millis)
       there was one(sqs).deleteMessage(qConf.url, msg.receiptHandle)
